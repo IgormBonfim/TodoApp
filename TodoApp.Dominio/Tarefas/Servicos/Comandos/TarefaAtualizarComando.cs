be3d671ae25ce308ef74ed2227be0f04a7278eca
IgorBonfim <igormbonfim@hotmail.com>
2023-02-12 11:36:58 -0300

    Adiciona TarefaAtualizarComando

---
+++ TodoApp.Dominio/Tarefas/Servicos/Comandos/TarefaAtualizarComando.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Dominio.Tarefas.Enumeradores;
+
+namespace TodoApp.Dominio.Tarefas.Servicos.Comandos
+{
+    public class TarefaAtualizarComando
+    {
+        public string Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Detalhes { get; set; }
+        public StatusTarefaEnum? Status { get; set; }
+    }
+}
