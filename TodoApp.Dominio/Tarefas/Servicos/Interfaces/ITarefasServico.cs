7ce770179b3347b363a0166ea04f84a61805158d
IgorBonfim <igormbonfim@hotmail.com>
2023-02-12 11:36:34 -0300

    Adiciona servico de tarefas

--- TodoApp.Dominio/Tarefas/Servicos/Interfaces/ITarefasServico.cs
+++ TodoApp.Dominio/Tarefas/Servicos/Interfaces/ITarefasServico.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TodoApp.Dominio.Tarefas.Entidades;
+using TodoApp.Dominio.Tarefas.Servicos.Comandos;
 
 namespace TodoApp.Dominio.Tarefas.Servicos.Interfaces
 {
     public interface ITarefasServico
     {
+        Tarefa Validar(string id);
         Tarefa Instanciar(string nome, string detalhes);
-        Tarefa Recuperar(string id);
+        Tarefa Atualizar(TarefaAtualizarComando comando);
         void Inserir(Tarefa tarefa);
+        void Excluir(string id);
     }
 }
--- TodoApp.Dominio/Tarefas/Servicos/TarefasServico.cs
+++ TodoApp.Dominio/Tarefas/Servicos/TarefasServico.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TodoApp.Dominio.Tarefas.Entidades;
+using TodoApp.Dominio.Tarefas.Enumeradores;
 using TodoApp.Dominio.Tarefas.Repositorios;
+using TodoApp.Dominio.Tarefas.Servicos.Comandos;
 using TodoApp.Dominio.Tarefas.Servicos.Interfaces;
 
 namespace TodoApp.Dominio.Tarefas.Servicos
@@ -18,6 +20,35 @@ namespace TodoApp.Dominio.Tarefas.Servicos
             this._tarefasRepositorio = tarefasRepositorio;
         }
 
+        public Tarefa Atualizar(TarefaAtualizarComando comando)
+        {
+            Tarefa tarefa = Validar(comando.Id);
+
+            if (tarefa.Status == StatusTarefaEnum.Concluida)
+                throw new Exception("Não é possível editar uma Tarefa concluída.");
+
+            if (comando.Nome != null)
+                tarefa.SetNome(comando.Nome);
+
+            if (comando.Detalhes != null)
+                tarefa.SetDetalhes(comando.Detalhes);
+
+            if (comando.Status != null)
+                tarefa.SetStatus(comando.Status.Value);
+
+            tarefa.SetDataAtualizacao();
+
+            if(comando.Status == StatusTarefaEnum.Concluida)
+            {
+                DateTime data = DateTime.Now;
+                tarefa.SetDataConclusao(data);
+            }
+
+            _tarefasRepositorio.Atualizar(comando.Id, tarefa);
+
+            return tarefa;
+        }
+
         public void Inserir(Tarefa tarefa)
         {
             _tarefasRepositorio.Adicionar(tarefa);
@@ -25,10 +56,10 @@ namespace TodoApp.Dominio.Tarefas.Servicos
 
         public Tarefa Instanciar(string nome, string detalhes)
         {
-            return new Tarefa(nome, detalhes);
+            return new Tarefa(nome, detalhes, StatusTarefaEnum.AguardandoInicio);
         }
 
-        public Tarefa Recuperar(string id)
+        public Tarefa Validar(string id)
         {
             Tarefa tarefa = _tarefasRepositorio.Recuperar(id);
 
@@ -36,6 +67,13 @@ namespace TodoApp.Dominio.Tarefas.Servicos
                 throw new Exception("Tarefa não encontrada.");
 
             return tarefa;
+        }
+
+        public void Excluir(string id)
+        {
+            Tarefa tarefa = Validar(id);
+
+            _tarefasRepositorio.Excluir(id);
         }
     }
 }
