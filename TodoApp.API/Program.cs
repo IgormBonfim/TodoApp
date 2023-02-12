e827c3543cc732b8691d5e3cce41e7bac22f94be
IgorBonfim <igormbonfim@hotmail.com>
2023-02-12 11:37:56 -0300

    Configura o cors

--- TodoApp.API/Program.cs
+++ TodoApp.API/Program.cs
@@ -20,6 +20,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(c =>
+{
+    c.AllowAnyHeader();
+    c.AllowAnyMethod();
+    c.AllowAnyOrigin();
+});
+
 app.UseAuthorization();
 
 app.MapControllers();
