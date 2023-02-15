5bd1f4c10a182c1cf2321045f6a0feed8b91a69f
IgorBonfim <igormbonfim@hotmail.com>
2023-02-15 20:59:01 -0300

    Implementa redução do acoplamento criando as configurações de database no container de dependencias

---
+++ TodoApp.Infra/Configs/MongoDatabaseConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Infra.Builders;
+using TodoApp.Infra.Configs.Interfaces;
+
+namespace TodoApp.Infra.Configs
+{
+    public class MongoDatabaseConfiguration : IMongoDatabaseConfiguration
+    {
+        public IMongoClient? MongoClient { get; set; }
+        public IMongoDatabase? MongoDatabase { get; set; }
+
+        public MongoDatabaseConfiguration ConfigureClient(string connectionString)
+        {
+            MongoClient = new MongoClient(connectionString);
+            return this;
+        }
+
+        public MongoDatabaseConfiguration ConfigureDatabaseName(string databaseName)
+        {
+            if (MongoClient == null)
+                throw new Exception("Client não configurado.");
+
+            MongoDatabase = MongoClient.GetDatabase(databaseName);
+            return this;
+        }
+    }
+}
---
+++ TodoApp.Infra/Configs/MongoDatabaseFluent.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Infra.Configs
+{
+    public class MongoDatabaseFluent
+    {
+        public static MongoDatabaseConfiguration Configure()
+        {
+            return new MongoDatabaseConfiguration();
+        }
+    }
+}
