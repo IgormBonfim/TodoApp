24a84e7755e2b6b095ba04bba604e65daedc1e42
IgorBonfim <igormbonfim@hotmail.com>
2023-02-19 10:28:55 -0300

    Adiciona controlador de testes

---
+++ TodoApp.API/Controllers/TesteController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TesteController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+        private readonly IWebHostEnvironment environment;
+
+        public TesteController(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            this.configuration = configuration;
+            this.environment = environment;
+        }
+
+        [HttpGet]
+        public IActionResult GetVariables()
+        {
+            IList<string> variables = new List<string>();
+
+            string ambiente = environment.EnvironmentName;
+            string connectionString = configuration.GetSection("DatabaseConfig:ConnectionString").Value;
+            string dataBaseName = configuration.GetSection("DatabaseConfig:DatabaseName").Value;
+
+            variables.Add(ambiente);
+            variables.Add(connectionString);
+            variables.Add(dataBaseName);
+
+            return Ok(variables);
+        }
+    }
+}
