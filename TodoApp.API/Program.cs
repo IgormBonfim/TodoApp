4aa11f5c7047d6c7bb1670ba62e2b33999817dda
IgorBonfim <igormbonfim@hotmail.com>
2023-02-19 00:37:10 -0300

    Implementa configuração de ambiente

--- TodoApp.API/Program.cs
+++ TodoApp.API/Program.cs
@@ -1,36 +1,57 @@
 using TodoApp.API.Middlewares;
 using TodoApp.Ioc;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.RegistrarServicos(builder.Configuration);
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-
-app.UseSwagger();
-app.UseSwaggerUI();
-
-
-app.UseMiddleware(typeof(TratamentoErrosGlobalMiddleware));
-
-app.UseCors(c =>
+namespace TodoApp.API
 {
-    c.AllowAnyHeader();
-    c.AllowAnyMethod();
-    c.AllowAnyOrigin();
-});
+    public class Program
+    {
+        public static IConfiguration Configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
 
-app.UseAuthorization();
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-app.MapControllers();
+            // Add services to the container.
 
-app.Run();
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
+            builder.Services.RegistrarServicos(Configuration);
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+
+            app.UseMiddleware(typeof(TratamentoErrosGlobalMiddleware));
+
+            app.UseCors(c =>
+            {
+                c.AllowAnyHeader();
+                c.AllowAnyMethod();
+                c.AllowAnyOrigin();
+            });
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+
+        }
+    }
+}
+
+
